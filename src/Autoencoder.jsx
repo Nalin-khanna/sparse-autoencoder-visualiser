71c4b798d9e49e20252366f3380d7347b2318dca
Nalin-khanna <nalinkhanna17@gmail.com>
2024-11-28 23:23:37 +0530

    temp

--- src/Autoencoder.jsx
+++ src/Autoencoder.jsx
@@ -249,7 +249,7 @@ const SparseAutoencoder = () => {
     </div>
         <button 
           onClick={processInput}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          className="px-4 py-2 bg-blue-300 text-white rounded hover:bg-blue-600"
         >
           Process Random Input
         </button>
