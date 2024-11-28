108d66bbad8a9d68f04cb79b8a62d4561a224e88
Swayam <swayambansal@outlook.com>
2024-11-28 23:01:32 +0530

    Improve ui and add User inputs
    Added user inputs, added 2 themes and improved ui, aded prettier config

---
+++ src/context/theme.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { createContext, useEffect, useState } from "react";
+
+export const ThemeContext = createContext("");
+
+export const ThemeProvider = ({ children }) => {
+  const [isLightTheme, setIsLightTheme] = useState(false);
+
+  const updateTheme = () => {
+    setIsLightTheme(!isLightTheme);
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = isLightTheme ? "#F5F5F5" : "#09090a";
+  }, [isLightTheme]);
+
+  return (
+    <ThemeContext.Provider value={{ isLightTheme, updateTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
